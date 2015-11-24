0769ed183a30d68da39284f095550b12191ca058
Izaak Meckler <ihmeckler@gmail.com>
2015-11-23 19:41:00 -0500

    bumped

--- Empty.elm
+++ Empty.elm
@@ -1,4 +1,8 @@
-module Empty where
+module Empty
+  ( Empty (..)
+  , elim
+  , yeahRight
+  ) where
 
 {-| Just a simple empty type.
 
@@ -10,6 +14,14 @@ module Empty where
 -}
 
 
+{-| It's impossible to construct a value of type `Empty`, because to do
+    so, you'd already need a value of type `Empty`!
+    
+    This type is useful for witnessing the impossibility of certain things.
+    For example, you could have a function which took a value of type
+    `Task Empty Int` which, the first type parameter to `Task` being the type
+    of errors encountered by the task, means it's impossible for that task to
+    fail. What a world we live in, eh? -}
 type Empty = Empty Empty
 
 {-| If `Empty` is inhabited then anything is. -}
@@ -20,6 +32,8 @@ elim (Empty e) = elim e
 yeahRight : Empty -> a
 yeahRight = elim
 
-type BertrandRusselIsThePope = BertrandRusselIsThePope BertrandRusselIsThePope
+type BertrandRusselIsThePope
+    = BertrandRusselIsThePope BertrandRusselIsThePope
+
 thenBertrandRusselIsThePope : Empty -> BertrandRusselIsThePope
 thenBertrandRusselIsThePope = elim
